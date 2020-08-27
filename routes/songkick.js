d3f347fff62b9849558aa7651bd88d12733dccf0
Harry <hheslin@gmail.com>
2020-08-27 17:14:30 +1000

    Passing formatted  Spotify API Data to pug render

--- routes/songkick.js
+++ routes/songkick.js
@@ -5,10 +5,17 @@ const qs = require('querystring');
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   console.log("Query Data!")
-  //const q = qs.parse(req.query);
-  console.log(req.query);
+  const data = req.query.spotify.split(",");
+
+  //Removing uneccesary data formatting 
+  data[0] = data[0].substring(2, data[0].length - 1);
+  for (i = 1; i < data.length; i++){
+    data[i] = data[i].substring(1, data[i].length - 1);
+  }
+  console.log(data);
+
   //res.send('Songkick Page');
-  res.render('index', {title: "Hello"});
+  res.render('index', {title: "Hello", spotify: data});
 });
 
 
--- routes/spotify.js
+++ routes/spotify.js
@@ -44,7 +44,7 @@ router.get('/authenticated', (req, res) => {
 });
 
 // Compile spotify data into query string for songkick API
-router.get('/songkickPage', (req, res) => {
+function songkickMashup(req, res) {
     const username = spotifyApiData[0].body.id;
     let artists = [username];
 
@@ -53,7 +53,7 @@ router.get('/songkickPage', (req, res) => {
         artists[i + 1] = spotifyApiData[1].data.items[i].images[0].url
     }
     res.redirect("/songkick?" + qs.stringify({ 'spotify': JSON.stringify(artists) }));
-})
+}
 
 // Request Spotfy data:Username & Top 10 user artists
 async function spotifyData(req, res, next) {
@@ -67,7 +67,7 @@ async function spotifyData(req, res, next) {
     try {
         spotifyApiData[0] = await spotifyApi.getMe();
         spotifyApiData[1] = await axios.get('https://api.spotify.com/v1/me/top/artists?time_range=short_term', options);
-        await res.redirect(`/songkickPage`);
+        songkickMashup(req, res);
     }
     catch (err) {
         console.log(err);
