4c4036f3c0943070dc92789ae4d0c6d80a5a8cbf
Harry <hheslin@gmail.com>
2020-09-01 18:18:55 +1000

    forward geocoder api semi implemented. Function ordering fixed, promises removed

--- routes/songkick.js
+++ routes/songkick.js
@@ -1,11 +1,13 @@
 var express = require('express');
+var opencage = require('./opencage.js');
 var router = express.Router();
 const qs = require('querystring');
-
 const Songkick = require('songkick-api-node');
+
 const songkickApi = new Songkick(process.env.SONGKICK_KEY);
 
 let topArtistsGigs = [];
+let anotherlist = [];
 let spotifyData;
 
 async function songkickData(req, res, next) {
@@ -24,25 +26,32 @@ async function songkickData(req, res, next) {
     }
   }
 
+  anotherlist = [];
   //Filtering concert results for shows occuring in Australia
-  for (i = 0; i < concertData.length; i+=2) {
+  for (i = 0; i < concertData.length; i+=3) {
     try {
       for (k = 0; k < concertData[i].length; k++) {
         if (concertData[i][k].venue.metroArea.country.displayName === "Australia") {
           topArtistsGigs.push(concertData[i][k]);
           topArtistsGigs.push(concertData[i + 1]);
+          anotherlist.push(await opencage.forwardGeocode(concertData[i][k].venue.displayName + ", Australia"));
         } 
       }
     } catch {
       console.log("No Australian Gigs found");
     }
   }
-
+  //console.log(anotherlist);
   for (i = 0; i < topArtistsGigs.length; i += 2) {
-    console.log(topArtistsGigs[i].displayName);
-    console.log(topArtistsGigs[i].venue.metroArea.state);
-    console.log(topArtistsGigs[i+1]);
+    // console.log(topArtistsGigs[i].displayName);
+    //console.log(topArtistsGigs[i]);
+    //console.log(topArtistsGigs[i+1]);
   }
+}
+
+async function retrieveData(req, res, next) {
+  await songkickData(req, res, next);
+  console.log(anotherlist);
   res.render('index', { title: "Hello", spotify: spotifyData, songkick: topArtistsGigs });
 }
 
@@ -56,7 +65,7 @@ router.get('/', function (req, res, next) {
     spotifyData[i] = spotifyData[i].substring(1, spotifyData[i].length - 1);
   }
   spotifyData[spotifyData.length - 1] = spotifyData[spotifyData.length - 1].substring(0, spotifyData[spotifyData.length - 1].length - 1);
-  songkickData(req, res, next) 
+  retrieveData(req, res, next);
  
 });
 
